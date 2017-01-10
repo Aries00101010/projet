527df40dc9583993fed6c70c11ed350569b17761
Aries00101010 <kevin.henkes@laposte.net>
2017-01-10 15:57:09 +0100

    Creation constructeur User

--- src/core/entities/User.java
+++ src/core/entities/User.java
@@ -5,4 +5,40 @@ public class User {
 	String prenom;
 	String login;
 	String motDePass;
+	
+	public User() {
+		
+	}
+	
+	public User(String nom, String prenom, String login, String motDePass) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.login = login;
+		this.motDePass = motDePass;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getMotDePass() {
+		return motDePass;
+	}
+	public void setMotDePass(String motDePass) {
+		this.motDePass = motDePass;
+	}
 }
