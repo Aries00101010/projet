480b96f3eafca8c08d00d0785eb6d929c2f697ed
romain bizeul <romain.bizeul@gmail.com>
2017-01-10 16:27:56 +0100

    creation de la classe Droits

--- src/core/entities/User.java
+++ src/core/entities/User.java
@@ -3,7 +3,7 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
-public class test {
+public class User {
 	String nom;
 	String prenom;
 	String login;
@@ -12,5 +12,38 @@ public class test {
 	
 	public User() {
 		this.droits = new ArrayList<String>();
+	
+	}
+	
+	public User(String nom, String prenom, String login, String motDePass) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.login = login;
+		this.motDePass = motDePass;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getMotDePass() {
+		return motDePass;
+	}
+	public void setMotDePass(String motDePass) {
+		this.motDePass = motDePass;
 	}
 }
