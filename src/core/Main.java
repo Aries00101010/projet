980013ca0d7287e8ebb96ae36ca3360b53ff03b8
romain bizeul <romain.bizeul@gmail.com>
2017-02-07 15:05:22 +0100

    suppression users & droits

--- src/core/Main.java
+++ src/core/Main.java
@@ -7,19 +7,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		User user1 = new User();
-		User user2 = new User();
-		
-		Droits droit1 = new Droits();
-		Droits droit2 = new Droits();
-		Droits droit3 = new Droits();
-		Droits droit4 = new Droits();
-		
-		user1.getDroits().add(droit1);
-		user1.getDroits().add(droit2);
-		user2.getDroits().add(droit3);
-		user2.getDroits().add(droit4);
-		
 	}
 
 }
