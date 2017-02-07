f56160548cd8d16d3d8bbaeaa36e1b939e66765d
romain bizeul <romain.bizeul@gmail.com>
2017-02-07 15:15:51 +0100

    script sql creation DB

---
+++ projet.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE projet;
+USE projet;
+
+
+#------------------------------------------------------------
+# Table: User
+#------------------------------------------------------------
+
+CREATE TABLE User(
+        idUser    int (11) Auto_increment  NOT NULL ,
+        nom       Varchar (25) ,
+        prenom    Varchar (25) ,
+        motDePass Varchar (25) ,
+        PRIMARY KEY (idUser )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: droit
+#------------------------------------------------------------
+
+CREATE TABLE droit(
+        idDroits int (11) Auto_increment  NOT NULL ,
+        niveau   Int ,
+        severite Int ,
+        nom      Varchar (25) ,
+        societe  Varchar (25) ,
+        PRIMARY KEY (idDroits )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: possède
+#------------------------------------------------------------
+
+CREATE TABLE possede(
+        idUser   Int NOT NULL ,
+        idDroits Int NOT NULL ,
+        PRIMARY KEY (idUser ,idDroits )
+)ENGINE=InnoDB;
+
+ALTER TABLE possede ADD CONSTRAINT FK_possede_idUser FOREIGN KEY (idUser) REFERENCES User(idUser);
+ALTER TABLE possede ADD CONSTRAINT FK_possede_idDroits FOREIGN KEY (idDroits) REFERENCES droit(idDroits);
