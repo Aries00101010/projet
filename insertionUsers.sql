9326e12fe57869b7d2a95a41a7d9ce1000ea7abb
romain bizeul <romain.bizeul@gmail.com>
2017-02-07 16:56:07 +0100

    Revert "suppression insertion sql"
    This reverts commit 9b254e25e6616af231f4f67aa33d04075f6e6e4a.

---
+++ insertionUsers.sql
@@ -0,0 +1,12 @@
+INSERT INTO user (nom, prenom, login, motDePass) VALUES ("Skywalker", "Luke", "LuckyShotsLeiaLikeAnAnimal", "Leia4ever"); 
+INSERT INTO user (nom, prenom, login, motDePass) VALUES ("Skywalker", "Anakin", "Mr.Vadoooor", "Jemelamort");
+
+INSERT INTO droit (niveau, severite, nom, societe) VALUES (10, 10, "Protéger Tatooine", "Ordre des Jedis");
+INSERT INTO droit (niveau, severite, nom, societe) VALUES (10, 10, "Détruire Tatooine", "Force obscure");
+INSERT INTO droit (niveau, severite, nom, societe) VALUES (10, 10, "Créer une étoile de la Pizza", "Force obscure");
+INSERT INTO droit (niveau, severite, nom, societe) VALUES (10, 10, "Organiser des réunions avec Darth Vador pour manger des pizzas", "Ordre des Jedis");
+
+INSERT INTO possede VALUES (1, 1);
+INSERT INTO possede VALUES (1, 4);
+INSERT INTO possede VALUES (2, 2);
+INSERT INTO possede VALUES (2, 3);
